32372eaf488a575df705d9866dc244e7cd370a57
Omar Barakat <70210346+3maro@users.noreply.github.com>
2023-08-10 21:55:30 +0200

    Delete Library.Identity directory

--- Library.Identity/Identity/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Library.Data.Identity
-{
-    public class ApplicationUser : IdentityUser
-    {
-    }
-}
--- Library.Identity/Identity/Response.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Library.API.Identity
-{
-    public class Response
-    {
-        public string Status { get; set; }
-        public string Message { get; set; }
-    }
-}
